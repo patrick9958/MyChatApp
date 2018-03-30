b900d36f444e38ea5f5ff62573cdd607fd9d5942
Patrick Neggie <patar9958@users.noreply.github.com>
2018-03-30 15:54:23 -0400

    Initial commit, gradle build successful

---
+++ app/src/main/java/com/android/example/mychatapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.android.example.mychatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
