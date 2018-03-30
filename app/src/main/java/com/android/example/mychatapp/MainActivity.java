0644040ab67999eb404019d3e6dd2dafc80b101d
Patrick Neggie <patar9958@users.noreply.github.com>
2018-03-30 16:30:44 -0400

    MainActivity updated to work with user-sign in with Email and message.xml added

--- app/src/main/java/com/android/example/mychatapp/MainActivity.java
+++ app/src/main/java/com/android/example/mychatapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.android.example.mychatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,56 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void displayChatMessages() {
+
+    }
+
+    if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+        // Start sign in/sign up activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .build(),
+                SIGN_IN_REQUEST_CODE
+        );
+    } else {
+        // User is already signed in. Therefore, display
+        // a welcome Toast
+        Toast.makeText(this,
+                "Welcome " + FirebaseAuth.getInstance()
+                        .getCurrentUser()
+                        .getDisplayName(),
+                Toast.LENGTH_LONG)
+                .show();
+
+        // Load chat room contents
+        displayChatMessages();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+
+                // Close the app
+                finish();
+            }
+        }
+
     }
 }
